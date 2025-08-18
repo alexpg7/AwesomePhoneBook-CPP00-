929128f17aeb94d06839a45e96e5b20c59b83b04
Alex Pascual <alex.pascualg7@gmail.com>
2025-08-18 15:34:10 +0200

    Add files via upload

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <cstring>
+#include <iostream>
+
+int	main(int narg, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (narg == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	i = 1;
+	while (i < narg)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			std::cout << (char)toupper(argv[i][j]);
+			j++;
+		}
+		std::cout << " ";
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
