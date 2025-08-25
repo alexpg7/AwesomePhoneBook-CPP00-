10126e15e2c33aa0d827727186e42ad6d193fef2
Alex Pascual <alex.pascualg7@gmail.com>
2025-08-25 13:01:43 +0200

    phonebook now trims spaces

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,5 +1,30 @@
 #include "PhoneBook.hpp"
 
+std::string	ft_trimtext(std::string comm)
+{
+	int			i = 0;
+	std::string	str;
+
+	while (comm[i])
+	{
+		if (comm[i] != ' ' && comm[i] != '\t')
+			str += comm[i];
+		i++;
+	}
+	return (str);
+}
+
+std::string	ft_trimfirst(std::string comm)
+{
+	int			i = 0;
+	std::string	str;
+
+	while (comm[i] == ' ' || comm[i] == '\t')
+		i++;
+	str = &comm[i];
+	return (str);
+}
+
 int	ft_read(std::string read, int lindex)
 {
 	if (read.length() == 1)
@@ -67,20 +92,45 @@ int	ft_add(PhoneBook &book)
 		else if (check == 0)
 			return (0);
 	}
-	std::cout << "Introduce NAME:" << std::endl;
-	if (!std::getline(std::cin, name))
-		return (-1);
-	std::cout << "Introduce LAST NAME:" << std::endl;
-	if (!std::getline(std::cin, lastname))
-		return (-1);
-	std::cout << "Introduce NICKNAME:" << std::endl;
-	if (!std::getline(std::cin, nickname))
-		return (-1);
+	while (1)
+	{
+		std::cout << "Introduce NAME:" << std::endl;
+		if (!std::getline(std::cin, name))
+			return (-1);
+		name = ft_trimfirst(name);
+		if (name.length() < 1)
+			std::cout << "This field cannot be empty." << std::endl;
+		else
+			break ;
+	}
+	while (1)
+	{
+		std::cout << "Introduce LAST NAME:" << std::endl;
+		if (!std::getline(std::cin, lastname))
+			return (-1);
+		lastname = ft_trimfirst(lastname);
+		if (lastname.length() < 1)
+			std::cout << "This field cannot be empty." << std::endl;
+		else
+			break ;
+	}
+	while (1)
+	{
+		std::cout << "Introduce NICKNAME:" << std::endl;
+		if (!std::getline(std::cin, nickname))
+			return (-1);
+		nickname = ft_trimfirst(nickname);
+		if (nickname.length() < 1)
+			std::cout << "This field cannot be empty." << std::endl;
+		else
+			break ;
+	}
 	while (true)
 	{
 		std::cout << "Introduce PHONE NUMBER:" << std::endl;
 		if (!std::getline(std::cin, number))
 			return (-1);
+		number = ft_trimtext(number);
 		check = ft_checknum(number);
 		if (check == 0)
 			break ;
@@ -89,9 +139,17 @@ int	ft_add(PhoneBook &book)
 		else if (check == 2)
 			std::cout << "Invalid PHONE NUMBER (must be of 9 digits)." << std::endl;
 	}
-	std::cout << "Introduce DARKEST SECRET:" << std::endl;
-	if (!std::getline(std::cin, secret))
-		return (-1);
+	while (1)
+	{
+		std::cout << "Introduce DARKEST SECRET:" << std::endl;
+		if (!std::getline(std::cin, secret))
+			return (-1);
+		secret = ft_trimfirst(secret);
+		if (secret.length() < 1)
+			std::cout << "This field cannot be empty." << std::endl;
+		else
+			break ;
+	}
 	book.add(name, lastname, nickname, number, secret);
 	return (0);
 }
@@ -117,6 +175,7 @@ int	main(void)
 			std::cout << "Something went wrong..." << std::endl;
 			return (ft_exit());
 		}
+		comm = ft_trimtext(comm);
 		if (comm == "EXIT")
 			return (ft_exit());
 		else if (comm == "ADD")
